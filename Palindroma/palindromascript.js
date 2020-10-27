1455adecdcdb69145431439921b5cd05c3e5a4c1
Emanuel <e.rista@outlook.it>
2020-10-27 18:57:43 +0100

    Funziona con gli alert (solo palindrome)

--- Palindroma/palindromascript.js
+++ Palindroma/palindromascript.js
@@ -1,17 +1,17 @@
-function strReverse(inputString) {
-  var arrChars = inputString.split(""); //trasforma in array
-  arrChars.reverse(); //invertire l'ordine degli elementi dell'array
-  return arChars.join("");//riunisce gli elementi dell'array che sono già stati invertiti. NB =("")=stringa vuota, altrimenti () unirebbe gli elementi con la virgola
-}
-function isPalindroma(inputString) { //funzione di check se è palindroma
-  var palindroma = false; //di default non è palindroma
-  if(inputString == strReverse(inputString)){//se (stringa in input) = (stringa in input invertita tramite la funzione strReverse) allora è true
-    palindroma = true;
-  }
-  return palindroma;
-}
-//test
-console.log("pirla", isPalindroma("pirla")); //dovrebbe dare false
-console.log("anna", isPalindroma("anna")); //dovrebbe dare true
+var parola = prompt('Inserisci una parola:'); //Il campo per inserire la parola
 
-// perchè non funziona? HELP Francesco & Sainato --> Ticket soon
+function reverseString(string) {
+
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var stringReverse = reverseArray.join("");
+    return stringReverse;
+} //questa funzione 1) separa le lettere della parola inserendole in un array
+//2) inverte l'ordine delle lettere che sono ora elementi di un array
+//3) riunisce le lettere in una parola. Alla fine le lettere della parola risultano invertite rispetto all'originale
+
+if (parola == reverseString(parola)){ //se la parola in input = parola con lettere invertite con la funzione reverseString
+  alert('La parola che hai inserito è palindroma!'); //allora fai apparire un alert che dice "è palindroma"
+} else { //per tutti gli altri casi
+  alert('La parola che hai inserito non è palindroma!');//fai apparire un alert che dice "non è palindroma"
+}
