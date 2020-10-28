9015a2be891abc6a4ea8e66dfbb9de4695250247
Emanuel <e.rista@outlook.it>
2020-10-28 16:17:14 +0100

    Funziona ma meglio la funzione

--- Pari e Dispari/pariedispariscript.js
+++ Pari e Dispari/pariedispariscript.js
@@ -45,9 +45,9 @@ if (sumResult % 2 == 0) {
 }
 //Se il fra ha scelto Pari e la somma è pari = "Complimenti hai vinto", se ha scelto Dispari e la somma è dispari "Complimenti hai vinto", diversamente "Hai perso"
 
-if (userChoicePariDispari = "Pari" && (sumResult % 2) = 0 {
+if (userChoicePariDispari == "Pari" && (sumResult % 2) == 0) {
   alert("Complimenti fra! Hai vinto!");
-} else if (userChoicePariDispari = "Dispari" && (sumResult % 2) = 1 {
+} else if (userChoicePariDispari == "Dispari" && (sumResult % 2) == 1) {
   alert("Complimenti fra! Hai vinto!");
 } else {
   alert("Hai perso fra! Sei scarso...");
