78cd8d84a058748d76bb95db2e26beaca95fb30a
Emanuel <e.rista@outlook.it>
2020-10-28 14:25:43 +0100

    Ok prompt pari o dispari + alert

---
+++ Pari e Dispari/pariedispariscript.js
@@ -0,0 +1,12 @@
+//Chiedo al fra di scegliere tra pari o dispari;
+var userChoicePariDispari = prompt("Ciao fra! Pari o Dispari?");
+//Voglio visualizzare un alert che mi dica "hai scelto pari" oppure "hai scelto dispari";
+if (userChoicePariDispari == "Pari") { //se la risposta è "Pari"
+  alert("Hai scelto Pari!");//allora mandami un alert che dica "Hai scelto Pari!"
+} else if (userChoicePariDispari == "Dispari") {//se invece la risposta è "Dispari"
+  alert("Hai scelto Dispari!");//allora mandami un alert che dica "Hai scelto Dispari!"
+} else {//diversamente
+  alert("Non ho capito fra...");//allora dì al fra che non hai capito
+}
+//Chiedo al fra di scegliere un numero tra 1 e 5
+var userChoiceNumber = prompt("")
